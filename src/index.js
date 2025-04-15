3c6a4d101eb792a693e05f85cc323678697a86ba
furstcontact <furstcontact@gmail.com>
2025-04-15 17:30:42 +0930

    JS not working, trying to solve

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.innerHTML = "Finding a quote for you...";
+}
+
+let quoteTopicElement = document.queryselector("quote-topic");
+quoteTopicElement.addEventListener("submit", generateQuote);
